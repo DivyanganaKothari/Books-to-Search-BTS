4823583d7bbd0c46488e9495a4b463a0d46ee246
divyangana <divyanganakothari1996@gmail.com>
2023-01-27 16:40:45 +0100

    commit

--- pages/About Us.py
+++ pages/About Us.py
@@ -11,13 +11,32 @@ col1.metric("Books", "1470")
 col2.metric("Source", "Kaggel")
 col3.metric("Accuracy","64%")
 
+option = st.selectbox(
+    'How would you like to be view the flow?',
+     ('Simple Flow of our Model', 'Detailed Flow of our Model'))
+if option== 'Simple Flow of our Model':
+    graph= graphviz.Digraph()
+    graph.edge('User','Input')
+    graph.edge('Input','Semantic Search','Word Embedding')
+    graph.edge('Semantic Search','Recommendation','Similarity Score')
+    graph.edge('Recommendation','Visualization')
+    graph.edge('Visualization','Input')
 
-graph= graphviz.Digraph()
-graph.edge('User','Input')
-graph.edge('Input','Semantic Search','Word Embedding')
-graph.edge('Semantic Search','Similarity Score')
-graph.edge('Similiarity Score','Recommendation','Rating')
-graph.edge('Recommendation','Visualization')
-graph.edge('Visualization','Input')
+    st.graphviz_chart(graph)
+else:
+    graph2= graphviz.Digraph()
+    graph2.edge('MySQL(Database)','Backend')
+    graph2.edge('Backend','Data Cleaning','Model')
+    graph2.edge('Data Cleaning','Pre-Trained Model(BERT)')
+    graph2.edge('Pre-Trained Model(BERT)','Embeddings')
+    graph2.edge('Embeddings','Semantic Search')
+    graph2.edge('Semantic Search','Recommendations','Top 10')
+    graph2.edge('User','Input')
+    graph2.edge('Input','Embeddings')
+    graph2.edge('Embeddings','Semantic Search','Similarity Score')
+    graph2.edge('Recommendations', 'Visualization')
+    graph2.edge('Visualization', 'Input')
 
-st.graphviz_chart(graph)
+    st.graphviz_chart(graph2)
+
+
