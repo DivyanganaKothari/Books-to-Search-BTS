3d76d6bafa42a2e02c7dca402b0e95e576b1b711
divyangana <divyanganakothari1996@gmail.com>
2023-01-31 02:12:21 +0100

    commit

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,43 +1,35 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+from config import GetDataFrameFromSqlQuery, GetSelectBooksQuery, GetSelectEmbeddingsQuery, SetStyle
 
 # Style
-st.set_page_config(layout='wide', initial_sidebar_state='expanded')
-with open('style.css') as css:
-    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+SetStyle(st)
 
 # Title
-#######
-st.title('Here you can get wto know more about our dataset')
-df= pd.read_csv('Data.xlsx - Merged Dataset_1.csv')
+title="Here you can get to know more about our dataset"
+st.markdown("<h1 style='text-align:center; padding:20px;' >" + title + "</h1>", unsafe_allow_html=True)
+dfBooks = GetDataFrameFromSqlQuery(GetSelectBooksQuery())
 #Average rating distribution for all books
-st.markdown('### Average rating distribution for all books')
-df.rating = df.rating.astype(float)
-fig3 = px.histogram(df, x='rating', nbins=50)#The number of bins (also known as intervals or classes) determines the granularity of the histogram.
+st.write('### Average rating distribution for all books')
+dfBooks.rating = dfBooks.rating.astype(float)
+fig3 = px.histogram(dfBooks, x='rating', nbins=50)#The number of bins (also known as intervals or classes) determines the granularity of the histogram.
 
 fig3.update_layout( xaxis_title='Average rating',
-                   font=dict(size=20))
-fig3.update_traces(marker=dict(color='#C58059'))
+                   font=dict(size=20),
+                   xaxis_tickangle=-45)
+fig3.update_traces(marker=dict(line=dict(color='black', width=1),
+                                color='#83c9ff'))
 st.plotly_chart(fig3)
 
-st.markdown('### Select a chart type in which you want to visualize the data')
-chart_type = st.selectbox('Select chart type', options=['Scatter', 'Line'])
+st.write('### Get to know our complete Data Set')
 col1, col2 = st.columns(2)
 
-x_axis_val = col1.selectbox('Select the X-axis', options=df.columns)
-y_axis_val = col2.selectbox('Select the Y-axis', options=df.columns)
+x_axis_val = col1.selectbox('Select the X-axis', options=dfBooks.columns)
+y_axis_val = col2.selectbox('Select the Y-axis', options=dfBooks.columns)
 
-if chart_type == 'Scatter':
-    plot = px.scatter(df, x=x_axis_val, y=y_axis_val)
-    plot.update_traces(marker=dict(color='#C58059'))
-    st.plotly_chart(plot, use_container_width=True)
-
-else:
-    plot = px.line(df, x=x_axis_val, y=y_axis_val)
-    plot.update_traces(marker=dict(color='#C58059'))
-    st.plotly_chart(plot, use_container_width=True)
+plot = px.scatter(dfBooks, x=x_axis_val, y=y_axis_val)
+plot.update_traces(marker=dict(color='#83c9ff'))
+st.plotly_chart(plot, use_container_width=True)
 
 
-#plot = px.scatter(df, x=x_axis_val, y=y_axis_val)
-#st.plotly_chart(plot, use_container_width=True)
